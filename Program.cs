ed3f206e447c274c38c3842f690a5faab2194af3
Ryan Bartholomew <rbartho1@my.westga.edu>
2025-02-22 21:02:30 -0500

    Removed the same period that I removed in release 2.0.0 so that the master branch and the release 2.0.0 branch were the same for now.

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddSingleton<CustomerOrdersData>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
