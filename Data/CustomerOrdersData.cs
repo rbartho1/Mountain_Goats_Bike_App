b9f53de242164ba9bb1a62109cdd0b33d9c79bbc
Ryan Bartholomew <rbartho1@my.westga.edu>
2025-03-07 14:00:22 -0500

    Changed the proc_cust_order_details procedure to include the integer for the customer id as a variable so the displayed order page would be unique to each customer and typed out the command to execute that command and display all of the selected customer's orders between January 1, 2016 and December 31, 2017 using their customer id to help retrieve that data.

--- Data/CustomerOrdersData.cs
+++ Data/CustomerOrdersData.cs
@@ -2,6 +2,7 @@
 using Mountain_Goats_Bike_App.Data;
 using Mountain_Goats_Bike_App.Models;
 using Microsoft.Data.SqlClient;
+using System.Data;
 
 namespace Mountain_Goats_Bike_App.Data
 {
@@ -15,46 +16,28 @@ namespace Mountain_Goats_Bike_App.Data
             using (var connection = new SqlConnection(_connectionString))
             {
                 connection.Open();
-
-                string sql = "SELECT [order_items].[order_id] AS order_id_number" +
-                    ",[item_id]" +
-                    ",[product_name]" +
-                    ",[orders].[order_date] AS date_ordered" +
-                    ",SUM(CONVERT(DECIMAL(10, 2), ROUND((1 - [discount]) *" +
-                    "([quantity] * [order_items].[list_price]), 2))) OVER (PARTITION BY [order_items].[order_id]) AS total_order_price" +
-                    ",[order_items].[list_price] AS unit_price" +
-                    ",[quantity], [first_name], [last_name]" +
-                    "FROM [BikeStores].[sales].[order_items]" +
-                    "JOIN [BikeStores].[sales].[orders]" +
-                    "ON [order_items].[order_id] = [orders].[order_id]" +
-                    "JOIN [BikeStores].[production].[products]" +
-                    "ON [order_items].[product_id] = [products].[product_id]" +
-                    "JOIN [BikeStores].[sales].[customers]" +
-                    "ON [orders].[customer_id] = [customers].[customer_id]" +
-                    "WHERE [orders].[customer_id] = @ID " +
-                    "AND [orders].[order_date] IN(SELECT TOP 3[orders].[order_date]" +
-                    "FROM [BikeStores].[sales].[orders] " +
-                    "WHERE [orders].[customer_id] = @ID " +
-                    "ORDER BY [orders].[order_date] DESC) " +
-                    "ORDER BY total_order_price DESC;";
-                SqlCommand command = new SqlCommand(sql, connection);
-                command.Parameters.AddWithValue("@ID", id);
-                using (SqlDataReader dataReader = command.ExecuteReader())
+                string exec_cust_order_proc =
+                "EXEC proc_cust_order_details '2016-01-01', '2017-12-31', " + id;
+                SqlCommand sqlProductSearchCommand = new SqlCommand(exec_cust_order_proc, connection);
+                using (SqlDataReader dataReader = sqlProductSearchCommand.ExecuteReader())
                 {
-                    while (dataReader.Read())
                     {
-                        CustomerOrders customerOrder = new CustomerOrders
-                        {   Order_id = (int)dataReader["order_id_number"],
-                            Unit_price = (decimal)dataReader["unit_price"],
-                            Item_id = (int)dataReader["item_id"],
-                            Quantity = (int)dataReader["quantity"],
-                            Order_date = DateOnly.FromDateTime((DateTime)dataReader["date_ordered"]),
-                            Item_name = dataReader["product_name"].ToString(),
-                            Total_order_price = (decimal)dataReader["total_order_price"],
-                            First_name = dataReader["first_name"].ToString(),
-                            Last_name = dataReader["last_name"].ToString(),
-                        };
-                        customerOrdersList.Add(customerOrder);
+                        while (dataReader.Read())
+                        {
+                            CustomerOrders customerOrder = new CustomerOrders
+                            {
+                                Order_id = (int)dataReader["order_id_number"],
+                                Unit_price = (decimal)dataReader["unit_price"],
+                                Item_id = (int)dataReader["item_id"],
+                                Quantity = (int)dataReader["quantity"],
+                                Order_date = DateOnly.FromDateTime((DateTime)dataReader["date_ordered"]),
+                                Item_name = dataReader["product_name"].ToString(),
+                                Total_order_price = (decimal)dataReader["total_order_price"],
+                                First_name = dataReader["first_name"].ToString(),
+                                Last_name = dataReader["last_name"].ToString(),
+                            };
+                            customerOrdersList.Add(customerOrder);
+                        }
                     }
                 }
             }
